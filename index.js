4d8f68b7b6057f7193df9b9a9dc559790de9ec8d
Flordeliza Mae Canlas <maemaeperlas@gmail.com>
2022-07-05 21:54:24 -0700

    commit tree 4e7315aaf3d9c552aeffefea7cd2ca47e0dd0168

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Add your code here
+
+const submitData = (name, email) => {
+    const items = {
+        name: name,
+        email: email,
+    }
+    
+    const configurationObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify(items)
+    }
+    return fetch("http://localhost:3000/users", configurationObj)
+    .then(res => res.json())
+    .then(data => {
+        const id = data.id;
+        const p = document.createElement("p");
+        p.textContent = id;
+        document.body.appendChild(p);
+    })
+    .catch(error => {
+        const err = error.message;
+        document.body.textContent = err;
+    })
+}
+
